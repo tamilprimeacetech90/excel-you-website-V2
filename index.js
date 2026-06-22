52cadc23c58f0f2d793f3b28d703d0fbdffa93ce
Excel You <tamilprimeacetech90@gmail.com>
2026-06-22 17:53:50 +0530

    fix render crash

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
 
 // =====================
 // 📤 UPLOAD ROUTES
