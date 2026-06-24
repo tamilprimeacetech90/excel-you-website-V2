71e435b30e492f7c67600c2a0ba5e84fd68a04de
Excel You <tamilprimeacetech90@gmail.com>
2026-06-24 10:18:33 +0530

    fix render crash

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ["user:email"]
     },
 
     async (accessToken, refreshToken, profile, done) => {
