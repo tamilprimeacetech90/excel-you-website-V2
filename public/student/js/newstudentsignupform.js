ee5b558f556255b9b2fdd4743af04f2d793ff1de
Excel You <tamilprimeacetech90@gmail.com>
2026-06-27 16:43:26 +0530

    Google signup fix

--- public/student/js/newstudentsignupform.js
+++ public/student/js/newstudentsignupform.js
@@ -243,6 +243,33 @@ if(signupForm){
 
             }
 
+            // =====================
+            // Toggle password
+            // =====================
+
+
+function togglePassword(inputId, button){
+
+    const input = document.getElementById(inputId);
+    const icon = button.querySelector("i");
+
+    if(input.type === "password"){
+
+        input.type = "text";
+
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+
+    }else{
+
+        input.type = "password";
+
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+
+    }
+
+}
 
             // =====================
             // BUTTON LOADING
